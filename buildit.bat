f811e0c927831c4e66a937baccfd39430da7e2ba
shrewdlogarithm <40507378+shrewdlogarithm@users.noreply.github.com>
2021-10-16 14:14:01 +0100

    Update buildit

--- buildit.bat
+++ buildit.bat
@@ -1,2 +1,2 @@
 pipreqs . --force
-pyinstaller poeclog.py --add-data "./poe.png;." --add-data "./templates;templates" --add-data "./css/*;." --add-data "./passive-skill-tree.json;." --onefile 
+pyinstaller poeclog.py --add-data "./poe.png;." --add-data "./templates;templates" --add-data "./css/*;." --add-data "./passive-skill-tree*.json;." --onefile 
